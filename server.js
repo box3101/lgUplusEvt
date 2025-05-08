05d1a675b4febfe60894eb6a7def7e77ca48e57e
box3101 <box4101@nate.com>
2025-05-08 13:39:10 +0900

    feat: add 2025 smart service support project page and assets

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 // 이벤트 페이지 라우팅
 const sgates = ["01", "02"];
 
-const isparks = ["01", "02", "03"];
+const isparks = ["01", "02", "03", "04"];
 
 sgates.forEach((sgateId) => {
   // PC 버전
